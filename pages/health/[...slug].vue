3200cf749fe9ba8847da1b804f0c1b11d4164001
jarlioGDev <130740111+jarlioGDev@users.noreply.github.com>
2023-06-18 22:41:56 +0300

    add margin

--- pages/health/[...slug].vue
+++ pages/health/[...slug].vue
@@ -1,21 +1,23 @@
 <template>
-  <header>
-    <nav>
-      <ul>
-        <li>
-          <NuxtLink to="/">Home</NuxtLink>
-        </li>
-        <li v-for="page in pages" :key="page._file">
-          <NuxtLink :to="page._path">
-            {{ page.title }}
-          </NuxtLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
-  <main>
-    <ContentDoc />
-  </main>
+  <div style="margin: 10px;">
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li v-for="page in pages" :key="page._file">
+            <NuxtLink :to="page._path">
+              {{ page.title }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <ContentDoc />
+    </main>
+  </div>
 </template>
 
 <script setup>
