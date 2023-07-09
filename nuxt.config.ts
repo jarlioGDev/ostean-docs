81535b55077b45d63bd749916572cc18dc7611a2
jarlioGDev <130740111+jarlioGDev@users.noreply.github.com>
2023-07-09 16:06:24 +0300

    hope fix deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/content', 'nuxt-headlessui', 'nuxt-icon', '@vueuse/nuxt'],
   ssr: false,
-  nitro: {
-    preset: 'service-worker'
-  },
   // Optionally change the default prefix.
   headlessui: {
     prefix: 'Headless'
